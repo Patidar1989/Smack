e82fbb33ffa7410fc65fd91a84661cf2b674207a
Pappulal Patidar <patidar@Pappulals-MacBook-Air.local>
2018-06-18 00:19:10 +0800

    lesson-02

---
+++ Smack/Controller/ChannelVC.swift
@@ -0,0 +1,21 @@
+//
+//  ChannelVC.swift
+//  Smack
+//
+//  Created by Pappulal Patidar on 17/06/2018.
+//  Copyright © 2018 Pappulal Patidar. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+    }
+
+
+
+}
